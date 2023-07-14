8ea976a4d6017c8ec39beb1ef43f46a76825e544
kodekev03 <131720665+kodekev03@users.noreply.github.com>
2023-07-14 13:37:53 -0700

    Create dataread.py

---
+++ dataread.py
@@ -0,0 +1,9 @@
+import csv
+
+with open('data.csv', 'r') as f:
+    reader = csv.reader(f)
+    headers = next(reader)  # Gets the first line
+    print(headers)
+
+
+// Name file uploaded to "data" in order for it to read the csv file
